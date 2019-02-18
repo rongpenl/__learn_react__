b9f56d8ed956bb9363dcb0474e28bb3793420b2d
rongpenl <rongpenl@usc.edu>
2019-02-18 14:15:14 -0800

    life cycle test

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -5,10 +5,34 @@ class Menu extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {   
+        this.state = {
             selectedDish: null
         }
+    }
 
+    onDishSelect(dish) {
+        this.setState(
+            { selectedDish: dish }
+        )
+    }
+
+    renderDish(dish) {
+        if (dish != null) {
+            return (
+                <Card>
+                    <CardImg width="100%" object src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        else {
+            return (
+                <div></div>
+            )
+        };
     }
 
     render() {
@@ -16,8 +40,10 @@ class Menu extends Component {
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card tag="li">
-                            <CardImg width="100%" object src={dish.image} alt={dish.name} />
+                    <Card onClick={
+                        () => this.onDishSelect(dish)
+                    }>
+                        <CardImg width="100%" object src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
                         </CardImgOverlay>
@@ -29,7 +55,10 @@ class Menu extends Component {
         return (
             <div className="container">
                 <div className="row">
-                        {menu}
+                    {menu}
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
 
