40bf629ceef3f9405d255bf8b3c1b102d8107767
rongpenl <rongpenl@usc.edu>
2019-02-18 21:03:56 -0800

    fixed misalignment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -32,20 +32,22 @@ const DishDetail = (props) => {
     const dish = props.dish;
     if (dish != null) {
         return (
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg width="100%" top src={dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>
-                                {dish.description}
-                            </CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    {renderComments(dish.comments)}
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <Card>
+                            <CardImg width="100%" top src={dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>
+                                    {dish.description}
+                                </CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        {renderComments(dish.comments)}
+                    </div>
                 </div>
             </div>
         );
